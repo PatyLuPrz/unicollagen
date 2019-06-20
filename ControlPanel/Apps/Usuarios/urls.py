6c6d65d2eb5b70cf26b6486fedfd3dd953cff431
Paty Prz <1717110752@utectulancingo.edu.mx>
2019-06-20 00:55:08 -0500

    Estructura de views para modulo usuarios

--- ControlPanel/Apps/Usuarios/urls.py
+++ ControlPanel/Apps/Usuarios/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from Apps.Productos.views import index
+from Apps.Productos.views import index, insert, delete, view, update
 
 urlpatterns = [
     path(r'', index),
+    path('insert', insert),
+    path('delete/', delete),
+    path('update/', update),
+    path('view/',view),
 ]
--- ControlPanel/Apps/Usuarios/views.py
+++ ControlPanel/Apps/Usuarios/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'Usuarios/index.html')
+
+def insert(request):
+    return render(request, 'Usuarios/insert.html')
+
+def view(request):
+    return render(request, 'Usuarios/view.html')
+
+def update(request):
+    return render(request, 'Usuarios/update.html')
+
+def delete(request):
+    return render(request, 'Usuarios/delete.html')
