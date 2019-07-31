c53b62e2bbbb98d79c3576626b8f5a5923d02b74
Paty Prz <1717110752@utectulancingo.edu.mx>
2019-07-31 13:50:32 -0500

    intento 3 de pasar a produccion

--- ControlPanel/ControlPanel/settings.py
+++ ControlPanel/ControlPanel/settings.py
@@ -20,11 +20,8 @@ from django.urls import reverse
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
